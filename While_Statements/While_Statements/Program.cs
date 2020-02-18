8c46b3c25bd4c67b6a1f83d3b7e3e8f0dd14ccee
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-18 15:32:59 -0500

    a

---
+++ While_Statements/While_Statements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace While_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old is the author of this program?\n");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 21;
+
+            do
+            {
+                switch (number)
+                {
+                    case 21:
+                        Console.WriteLine("\nThat was correct! I am 21 years old.");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("\nYou are incorrect, try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+    }
+}
