ca2d9bf4e37f9f2a68c6cc6e7eaed863c32e3192
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-20 15:35:11 -0500

    classes_5

---
+++ Classes_5/Classes_5/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_5
+{
+    class Operations
+    {
+
+        public void Div(int num)
+        {
+            int quotient = num / 2;
+            Console.WriteLine("{0} / 2 = {1}", num, quotient);
+        }
+
+        public void Greeting()
+        {
+            Console.WriteLine("\nHello, World! The method that produced this text took no parameters");
+        }
+
+        public int stringOrInt(int numMult)
+        {
+            int product = numMult * 5;
+            return product;
+        }
+
+        public string stringOrInt(string name)
+        {
+            string greet = "Hello, " + name;
+            return greet;
+        }
+
+        public static void withoutObj()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ Classes_5/Classes_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Classes_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Operations n = new Operations();
+
+            Console.WriteLine("Please type an integer to be divided by 2:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            n.Div(num);
+
+            Console.ReadLine();
+
+            n.Greeting();
+
+            Console.ReadLine();
+
+            Console.WriteLine("Please type an integer to be multiplied by 5:");
+            int numMult = Convert.ToInt32(Console.ReadLine());
+            int callMult;
+            callMult = n.stringOrInt(numMult);
+            Console.WriteLine(callMult);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Please type a name: ");
+            string nameGreet = Console.ReadLine();
+            string callGreet;
+            callGreet = n.stringOrInt(nameGreet);
+            Console.WriteLine(callGreet);
+
+            Console.ReadLine();
+
+            Operations.withoutObj();
+
+            Console.ReadKey();
+
+        }
+    }
+}
