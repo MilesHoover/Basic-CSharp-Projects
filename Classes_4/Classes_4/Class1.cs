78ec1f00a3fd4b7a24c92e19b8ed464cf9e2b224
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-20 14:29:57 -0500

    classes_4

---
+++ Classes_4/Classes_4/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_4
+{
+    class Class1
+    {
+        public void numOp(int num1, int num2)
+        {
+            int prod = num1 * 5;
+            Console.WriteLine(prod);
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ Classes_4/Classes_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 n = new Class1();
+
+            Console.WriteLine("Please type a number to be multiplied by 5.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nPlease type another number to be displayed on screen.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            n.numOp(num1, num2);
+
+            Console.ReadLine();
+        }
+    }
+}
