d7ff8b0f5b20577389c38808a7dd8a15e7c5f424
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-20 11:50:10 -0500

    classes_3

---
+++ Classes_3/Classes_3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_3
+{
+    class MathOp
+    {
+        public int Mult(int num1, int num2 = 0)
+        {
+            int numTotal = num1 + num2;
+            int prod = numTotal * 5;
+            return prod;
+        }
+    }
+}
---
+++ Classes_3/Classes_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Classes_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int callMath;
+            MathOp n = new MathOp();
+
+            Console.WriteLine("Please type a number to be multiplied by 5:\n");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nWould you like to add another number to the previous number? (yes/no)\n");
+            string answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                Console.WriteLine("\nPlease type the second number:\n");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                callMath = n.Mult(num1, num2);
+                Console.WriteLine("{0} + {1} * 5 = {2}", num1, num2, callMath);
+            }
+            else if (answer == "no")
+            {
+                callMath = n.Mult(num1);
+                Console.WriteLine("{0} * 5 = {1}", num1, callMath);
+            }
+            else
+            {
+                Console.WriteLine("\nPlease try again and answer with either \"yes\" or \"no\"\nProgram will now close");
+            }
+            Console.ReadLine();
+        }
+    }
+}
