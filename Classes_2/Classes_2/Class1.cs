1767ac0d283ba1b5e612fef9097e4ebf2a6e4d56
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-20 10:59:24 -0500

    asdf

---
+++ Classes_2/Classes_2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_2
+{
+    class mathOp
+    {
+        public int Mult(int numberMult)
+        {
+            int multProd = numberMult * 5;
+            return multProd;
+        }
+
+        public int Mult(decimal numberSub)
+        {
+            decimal subProd = numberSub - 5;
+            int intProd = Convert.ToInt32(subProd);
+            return intProd;
+        }
+
+        public int Mult(string numberString)
+        {
+            int stringCon = Convert.ToInt32(numberString);
+            int addProd = stringCon + 5;
+            return addProd;
+        }
+    }
+}
---
+++ Classes_2/Classes_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int callMath;
+            mathOp n = new mathOp();
+
+            Console.WriteLine("Please type a number you wish to be multiplied by 5:");
+            int numberMult = Convert.ToInt32(Console.ReadLine());
+            callMath = n.Mult(numberMult);
+            Console.WriteLine("\n{0} * 5 = {1}", numberMult, callMath);
+            
+            Console.ReadLine();
+
+            Console.WriteLine("\nPlease type a number you wish to be subtracted by 5:");
+            decimal numberSub = Convert.ToDecimal(Console.ReadLine());
+            callMath = n.Mult(numberSub);
+            Console.WriteLine("\n{0} - 5 = {1}", numberSub, callMath);
+
+            Console.ReadLine();
+
+            Console.WriteLine("\nPlease type a number you wish to have 5 added to:");
+            string numberString = Console.ReadLine();
+            callMath = n.Mult(numberString);
+            Console.WriteLine("\n{0} + 5 = {1}", numberString, callMath);
+
+            Console.ReadLine();
+        }
+    }
+}
