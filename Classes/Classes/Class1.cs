cadcd2fec86f7873ca343f690e2e1b168338e14e
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-20 10:11:05 -0500

    classes and Methods

---
+++ Classes/Classes/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Operations
+    {
+        public int OperationAdd(int intAdd)
+        {
+            int sum = intAdd + 50;
+            return sum;
+        }
+
+        public int OperationSub(int intSub)
+        {
+            int dif = intSub - 50;
+            return dif;
+        }
+
+        public int OperationMult(int intMult)
+        {
+            int prod = intMult * 50;
+            return prod;
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations n = new Operations();
+
+            Console.WriteLine("What number would you like added by 50?\n");
+            int intAdd = Convert.ToInt32(Console.ReadLine());
+            int sum = n.OperationAdd(intAdd);
+            Console.WriteLine("The sum is: " + sum);
+
+            Console.WriteLine("\nWhat number would you like subtracted by 50?\n");
+            int intSub = Convert.ToInt32(Console.ReadLine());
+            int dif = n.OperationSub(intSub);
+            Console.WriteLine("The difference is: " + dif);
+
+            Console.WriteLine("\nWhat number would you like multiplied by 50?\n");
+            int intMult = Convert.ToInt32(Console.ReadLine());
+            int prod = n.OperationMult(intMult);
+            Console.WriteLine("The product is: " + prod);
+
+            Console.ReadLine();
+        }
+    }
+}
