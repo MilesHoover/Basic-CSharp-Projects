91f9e3d89558bc8343f6f87bb706e721459fabdd
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-24 09:46:35 -0500

    classes_6

---
+++ Classes_6/Classes_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee a = new Employee();
+            a.FirstName = "Sample";
+            a.LastName = "Student";
+            a.SayName();
+            Console.ReadLine();
+        }
+    }
+}
